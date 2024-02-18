f5e0a1e5161de9e4d8491a57f9bc0b101ab47f7e
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-18 16:44:29 +0100

    Modifica dei metodi dell'interfaccia INoleggiabile

--- src/it/unipv/sfw/rentacar/model/veicolo/noleggio/INoleggiabile.java
+++ src/it/unipv/sfw/rentacar/model/veicolo/noleggio/INoleggiabile.java
@@ -2,7 +2,6 @@ package it.unipv.sfw.rentacar.model.veicolo.noleggio;
 
 public interface INoleggiabile {
 
-	public Noleggio getStatoNoleggio();
-	public void setStatoNoleggio(Noleggio statoNoleggio);
+	public boolean noleggiabile();
 	
 }
