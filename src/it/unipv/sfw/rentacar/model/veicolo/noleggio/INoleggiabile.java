1d50f0d992b04d497a4876b8c99a619f2d7d24d9
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-18 16:29:48 +0100

    Creazione interfaccia INoleggiabile

---
+++ src/it/unipv/sfw/rentacar/model/veicolo/noleggio/INoleggiabile.java
@@ -0,0 +1,8 @@
+package it.unipv.sfw.rentacar.model.veicolo.noleggio;
+
+public interface INoleggiabile {
+
+	public Noleggio getStatoNoleggio();
+	public void setStatoNoleggio(Noleggio statoNoleggio);
+	
+}
