444bec9afaca877367ff60b6e5d73895b6302e04
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-20 17:59:32 +0100

    Creazione metodo controllo numero patente

--- src/it/unipv/sfw/rentacar/model/utenti/documenti/Patente.java
+++ src/it/unipv/sfw/rentacar/model/utenti/documenti/Patente.java
@@ -2,6 +2,10 @@ package it.unipv.sfw.rentacar.model.utenti.documenti;
 
 import java.util.Arrays;
 import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import it.unipv.sfw.rentacar.model.exception.NumeroPatenteInvalidoException;
 
 public class Patente {
 	
@@ -9,7 +13,12 @@ public class Patente {
 	private Date scadenza;
 	private String[] categorie;
 	
-	public Patente(String numero, Date scadenza, String[] categorie) {
+	public Patente(String numero, Date scadenza, String[] categorie) throws NumeroPatenteInvalidoException {
+		
+		if (!verificaNumeroPatente(numero)) {
+			throw new NumeroPatenteInvalidoException();
+		}
+		
 		this.numero = numero;
 		this.scadenza = scadenza;
 		this.categorie = categorie;
@@ -18,21 +27,32 @@ public class Patente {
 	public String getNumero() {
 		return numero;
 	}
-
+	
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
-
+	
+	
 	public Date getScadenza() {
 		return scadenza;
 	}
-
+	
 	public void rinnova(Date rinnovo) {
 		this.scadenza = rinnovo;
 	}
-
+	
 	public String[] getCategorie() {
 		return categorie;
+	}
+	
+	public boolean verificaNumeroPatente(String numeroPatente) {
+		
+		String formatoPatente = "^[A-Za-z]{2}\\d{6}+[A-Za-z0-9]{2}$";
+		
+		Pattern pattern = Pattern.compile(formatoPatente);
+		Matcher matcher = pattern.matcher(numeroPatente);
+		
+		return matcher.matches();
 	}
 
 	@Override
@@ -40,5 +60,5 @@ public class Patente {
 		return "Patente [numero=" + numero + ", scadenza=" + scadenza + ", categorie=" + Arrays.toString(categorie)
 				+ "]";
 	}
-
+	
 }
