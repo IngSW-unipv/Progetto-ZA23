b5b5362121c0faab87cb2a07e8ae5cfbcab244fc
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-21 22:38:40 +0100

    Implementazione controlli sugli attributi

--- src/it/unipv/sfw/rentacar/model/veicolo/Auto.java
+++ src/it/unipv/sfw/rentacar/model/veicolo/Auto.java
@@ -1,5 +1,9 @@
 package it.unipv.sfw.rentacar.model.veicolo;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import it.unipv.sfw.rentacar.model.exception.TargaNonValidaException;
 import it.unipv.sfw.rentacar.model.veicolo.caratteristiche.CaratteristicheTecniche;
 import it.unipv.sfw.rentacar.model.veicolo.noleggio.INoleggiabile;
 import it.unipv.sfw.rentacar.model.veicolo.noleggio.Noleggio;
@@ -13,13 +17,31 @@ public class Auto implements INoleggiabile{
 	private double costoNoleggioGiornaliero;
 	private Noleggio statoNoleggio;
 	
-	public Auto(String targa, String marca, String modello, CaratteristicheTecniche caratteristicheTecniche, double costoNoleggioGiornaliero , Noleggio statoNoleggio) {
+	public Auto(String targa, String marca, String modello, CaratteristicheTecniche caratteristicheTecniche, double costoNoleggioGiornaliero , Noleggio statoNoleggio) throws TargaNonValidaException {
+		
+		if (!verificaTarga(targa)) {
+			throw new TargaNonValidaException();
+		}
+		
+		if (marca.length() <= 0) {
+			throw new IllegalArgumentException("Il campo marca non può essere vuoto");
+		}
+		
+		if (modello.length() <= 0) {
+			throw new IllegalArgumentException("Il campo modello non può essere vuoto");
+		}
+		
+		if (costoNoleggioGiornaliero <= 0) {
+			throw new IllegalArgumentException("Il campo costoNoleggioGiornaliero non può essere nullo o negativo");
+		}
+		
+		
 		this.targa = targa;
 		this.marca = marca;
 		this.modello = modello;
 		this.caratteristicheTecniche = caratteristicheTecniche;
 		this.costoNoleggioGiornaliero = costoNoleggioGiornaliero;
-		this.statoNoleggio = statoNoleggio;
+		this.statoNoleggio = Noleggio.DISPONIBILE;
 	}
 	
 	public String getTarga() {
@@ -75,6 +97,14 @@ public class Auto implements INoleggiabile{
 			this.statoNoleggio = Noleggio.NOLEGGIATA;
 		}else
 			this.statoNoleggio = Noleggio.DISPONIBILE;
+	}
+	
+	public boolean verificaTarga(String targa) {
+		String formatoTarga = "[A-HJ-NP-TV-Z]{2}[0-9]{3}[A-HJ-NP-TV-Z]{2}[0-9]{2}";
+		Pattern pattern = Pattern.compile(formatoTarga);
+		Matcher matcher = pattern.matcher(targa);
+		
+		return matcher.matches();
 	}
 	
 	@Override
