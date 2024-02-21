428441eef0999a28e883031572f5f7d7e8fdf68a
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-21 23:25:16 +0100

    Aggiunta metodi di controllo degli attributi

--- src/it/unipv/sfw/rentacar/model/veicolo/caratteristiche/CaratteristicheTecniche.java
+++ src/it/unipv/sfw/rentacar/model/veicolo/caratteristiche/CaratteristicheTecniche.java
@@ -1,5 +1,6 @@
 package it.unipv.sfw.rentacar.model.veicolo.caratteristiche;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 
 public class CaratteristicheTecniche {
@@ -13,6 +14,27 @@ public class CaratteristicheTecniche {
 	
 	public CaratteristicheTecniche(int annoProduzione, Cambio tipoCambio, Carburante[] tipoCarburante, int postiAuto,
 			int cilindrata, int potenza) {
+		
+		if (annoProduzione > LocalDate.now().getYear()) {
+			throw new IllegalArgumentException("L'anno di produzione non può essere maggiore dell'anno corrente");
+		}
+		
+		if (tipoCambio == null || tipoCarburante == null) {
+			throw new NullPointerException("La tipologia di cambio o la tipologia di carburante non può essere null");
+		}
+
+		if (postiAuto <= 0) {
+			throw new IllegalArgumentException("I posti auto non possono essere negativi o pari a 0");
+		}
+		
+		if (cilindrata <= 0) {
+			throw new IllegalArgumentException("La cilindrata non può essere negativa o pari a 0");
+		}
+		
+		if (potenza <= 0) {
+			throw new IllegalArgumentException("La potenza non può essere negativa o pari a 0");
+		}
+		
 		this.annoProduzione = annoProduzione;
 		this.tipoCambio = tipoCambio;
 		this.tipoCarburante = tipoCarburante;
@@ -75,8 +97,6 @@ public class CaratteristicheTecniche {
 				+ ", tipoCarburante=" + Arrays.toString(tipoCarburante) + ", postiAuto=" + postiAuto + ", cilindrata="
 				+ cilindrata + ", potenza=" + potenza + "]";
 	}
-	
-	
 	
 	
 	
