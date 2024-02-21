bca1211ed9e463a59a8188a6776ad62bae8ae901
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-21 23:34:28 +0100

    Implementazione controllo degli attributi

--- src/it/unipv/sfw/rentacar/model/utenti/Utente.java
+++ src/it/unipv/sfw/rentacar/model/utenti/Utente.java
@@ -8,7 +8,39 @@ public abstract class Utente {
 	private String password;
 	
 	public Utente(String nome, String cognome, String username, String password) {
-		super();
+		
+		if (nome.equals(null)) {
+			throw new NullPointerException("Nome non può essere null");
+		}
+		
+		if (cognome.equals(null)) {
+			throw new NullPointerException("Cognome non può essere null");
+		}
+		
+		if (username.equals(null)) {
+			throw new NullPointerException("Username non può essere null");
+		}
+		
+		if (password.equals(null)) {
+			throw new NullPointerException("Password non può essere null");
+		}
+		
+		if (nome.length() <= 0) {
+			throw new IllegalAccessError("Campo Nome vuoto");
+		}
+		
+		if (cognome.length() <= 0) {
+			throw new IllegalAccessError("Campo Cognome vuoto");
+		}
+		
+		if (username.length() <= 0) {
+			throw new IllegalAccessError("Campo Username vuoto");
+		}
+		
+		if (password.length() <= 0) {
+			throw new IllegalAccessError("Campo Password vuoto");
+		}
+		
 		this.nome = nome;
 		this.cognome = cognome;
 		this.username = username;
