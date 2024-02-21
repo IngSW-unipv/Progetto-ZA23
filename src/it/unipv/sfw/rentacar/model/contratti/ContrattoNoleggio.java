0e5de3269617971faf06c3585f729487708d6b30
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-21 21:07:00 +0100

    Implementazione metodo creazioneContratto e incrementaID

---
+++ src/it/unipv/sfw/rentacar/model/contratti/ContrattoNoleggio.java
@@ -0,0 +1,43 @@
+package it.unipv.sfw.rentacar.model.contratti;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import it.unipv.sfw.rentacar.model.contratti.pagamenti.Pagamento;
+import it.unipv.sfw.rentacar.model.utenti.Cliente;
+import it.unipv.sfw.rentacar.model.veicolo.Auto;
+
+public class ContrattoNoleggio {
+
+	private static int ID = 0;
+	private String idContratto;
+	private Cliente cliente;
+	private Auto auto;
+	private LocalDate inizioNoleggio;
+	private LocalDate fineNoleggio;
+	private double importo;
+	private Pagamento pagamento;
+	
+	public ContrattoNoleggio(Cliente cliente, Auto auto, String inizioNoleggio, String fineNoleggio, double importo, Pagamento pagamento) {
+		this.idContratto = creazioneIDContratto();
+		this.cliente = cliente;
+		this.auto = auto;
+		
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+		this.inizioNoleggio = LocalDate.parse(inizioNoleggio, formatter);
+		this.fineNoleggio = LocalDate.parse(fineNoleggio, formatter);
+		this.importo = 0;
+		incrementaID();
+	}
+	
+	public void incrementaID() {
+		ID++;
+	}
+	
+	public String creazioneIDContratto() {
+		return String.format("Rent-a-Car#%05d", ID);
+	}
+	
+	
+	
+}
