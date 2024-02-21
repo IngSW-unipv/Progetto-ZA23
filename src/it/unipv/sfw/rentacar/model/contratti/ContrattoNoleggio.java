8636fddfda958d5779bdb45c4845586b88331cb8
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-21 21:29:38 +0100

    Implementazione calcolaNoleggio

--- src/it/unipv/sfw/rentacar/model/contratti/ContrattoNoleggio.java
+++ src/it/unipv/sfw/rentacar/model/contratti/ContrattoNoleggio.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.ChronoUnit;
 
+import it.unipv.sfw.rentacar.model.contratti.pagamenti.CartaDiCredito;
 import it.unipv.sfw.rentacar.model.contratti.pagamenti.Pagamento;
 import it.unipv.sfw.rentacar.model.utenti.Cliente;
 import it.unipv.sfw.rentacar.model.veicolo.Auto;
@@ -19,18 +20,83 @@ public class ContrattoNoleggio {
 	private double importo;
 	private Pagamento pagamento;
 	
-	public ContrattoNoleggio(Cliente cliente, Auto auto, String inizioNoleggio, String fineNoleggio, double importo, Pagamento pagamento) {
+	public ContrattoNoleggio(Cliente cliente, Auto auto, String inizioNoleggio, String fineNoleggio, double importo, CartaDiCredito pagamento) {
 		this.idContratto = creazioneIDContratto();
 		this.cliente = cliente;
 		this.auto = auto;
-		
 		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy");
 		this.inizioNoleggio = LocalDate.parse(inizioNoleggio, formatter);
 		this.fineNoleggio = LocalDate.parse(fineNoleggio, formatter);
-		this.importo = conteggioGiorniNoleggio();
+		this.importo = calcolaNoleggio();
+		this.pagamento = pagamento;
+		
 		incrementaID();
 	}
 	
+	public static int getID() {
+		return ID;
+	}
+
+	public static void setID(int iD) {
+		ID = iD;
+	}
+
+	public String getIdContratto() {
+		return idContratto;
+	}
+
+	public void setIdContratto(String idContratto) {
+		this.idContratto = idContratto;
+	}
+
+	public Cliente getCliente() {
+		return cliente;
+	}
+
+	public void setCliente(Cliente cliente) {
+		this.cliente = cliente;
+	}
+
+	public Auto getAuto() {
+		return auto;
+	}
+
+	public void setAuto(Auto auto) {
+		this.auto = auto;
+	}
+
+	public LocalDate getInizioNoleggio() {
+		return inizioNoleggio;
+	}
+
+	public void setInizioNoleggio(LocalDate inizioNoleggio) {
+		this.inizioNoleggio = inizioNoleggio;
+	}
+
+	public LocalDate getFineNoleggio() {
+		return fineNoleggio;
+	}
+
+	public void setFineNoleggio(LocalDate fineNoleggio) {
+		this.fineNoleggio = fineNoleggio;
+	}
+
+	public double getImporto() {
+		return importo;
+	}
+
+	public void setImporto(double importo) {
+		this.importo = importo;
+	}
+
+	public Pagamento getPagamento() {
+		return pagamento;
+	}
+
+	public void setPagamento(Pagamento pagamento) {
+		this.pagamento = pagamento;
+	}
+
 	public void incrementaID() {
 		ID++;
 	}
@@ -41,6 +107,10 @@ public class ContrattoNoleggio {
 	
 	public int conteggioGiorniNoleggio() {
 		return (int)ChronoUnit.DAYS.between(inizioNoleggio, fineNoleggio);
+	}
+	
+	public double calcolaNoleggio() {
+		return conteggioGiorniNoleggio() * this.auto.getCostoNoleggioGiornaliero();
 	}
 	
 }
