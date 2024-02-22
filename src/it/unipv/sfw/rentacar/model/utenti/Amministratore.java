746217134965c81c481d5dc63a0c9b344015a38b
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-22 19:50:53 +0100

    Implementazione Classe Amministratore

---
+++ src/it/unipv/sfw/rentacar/model/utenti/Amministratore.java
@@ -0,0 +1,14 @@
+package it.unipv.sfw.rentacar.model.utenti;
+
+import it.unipv.sfw.rentacar.model.agenzia.AgenziaNoleggioAuto;
+import it.unipv.sfw.rentacar.model.veicolo.Auto;
+
+public class Amministratore extends Utente{
+
+	public Amministratore(String nome, String cognome, String username, String password) {
+		super(nome, cognome, username, password);
+	}
+
+
+	
+}
