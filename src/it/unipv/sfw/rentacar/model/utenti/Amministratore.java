afb88b15ca67fc85e67da616cf10a70c029ae0d6
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-22 19:51:20 +0100

    Implementazione metodi di aggiunta e rimozione auto

--- src/it/unipv/sfw/rentacar/model/utenti/Amministratore.java
+++ src/it/unipv/sfw/rentacar/model/utenti/Amministratore.java
@@ -8,7 +8,18 @@ public class Amministratore extends Utente{
 	public Amministratore(String nome, String cognome, String username, String password) {
 		super(nome, cognome, username, password);
 	}
-
-
 	
+	public void aggiungiAuto(AgenziaNoleggioAuto agenzia, Auto a) {
+		agenzia.getElencoAuto().add(a);
+	}
+	
+	public void rimuoviAuto(AgenziaNoleggioAuto agenzia, Auto a) {
+		agenzia.getElencoAuto().remove(a);
+	}
+	
+	@Override
+	public String toString() {
+		return "Amministratore [nome=" + getNome() + ", cognome=" + getCognome() + ", Username="
+				+ getUsername() + ", password()=" + getPassword() + "]";
+	}
 }
