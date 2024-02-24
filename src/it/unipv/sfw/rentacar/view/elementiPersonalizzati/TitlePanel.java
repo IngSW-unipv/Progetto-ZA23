07275d23092952c1df7539ec3cac8a1f1021c56e
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-24 14:01:52 +0100

    Implementazione componente personalizzato "TitlePanel"

---
+++ src/it/unipv/sfw/rentacar/view/elementiPersonalizzati/TitlePanel.java
@@ -0,0 +1,35 @@
+package it.unipv.sfw.rentacar.view.elementiPersonalizzati;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Font;
+import java.io.IOException;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class TitlePanel extends JPanel{
+
+	private ImageIcon imageIcon;
+	private JLabel imageLabel;
+	private JLabel titleLabel;
+	
+	public TitlePanel() throws IOException {
+		
+		setLayout(new BorderLayout());
+		setBackground(Color.CYAN);
+		
+        imageIcon = new ImageIcon("src\\it\\unipv\\sfw\\rentacar\\resources\\logo_trasparente2.png");
+        imageLabel = new JLabel(imageIcon);
+        add(imageLabel, BorderLayout.WEST);
+        
+        titleLabel = new JLabel("Rent-a-Car");
+        titleLabel.setFont(new Font("Brush Script MT", Font.BOLD, 50));
+        titleLabel.setForeground(Color.BLUE);
+        setBorder(BorderFactory.createLineBorder(Color.BLACK, 1));
+        add(titleLabel, BorderLayout.CENTER);
+        
+	}
+	
+}
