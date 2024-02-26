8f4a053b3a4474ec955504d1073cce5a060857c4
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-02-26 15:09:31 +0100

    Aggiunta ButtonHome e modifica dimensioni pulsanti

--- src/it/unipv/sfw/rentacar/view/elementiPersonalizzati/SideBarPanel.java
+++ src/it/unipv/sfw/rentacar/view/elementiPersonalizzati/SideBarPanel.java
@@ -1,6 +1,8 @@
 package it.unipv.sfw.rentacar.view.elementiPersonalizzati;
 
 import java.awt.Color;
+import java.awt.Dimension;
+
 import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
@@ -11,19 +13,32 @@ import javax.swing.border.CompoundBorder;
 
 public class SideBarPanel extends JPanel{
 
+	private JLabel homeLabel;
 	private JLabel noleggioLabel;
+	private JButton homeButton;
 	private JButton noleggioButton;
+	
 	
 	public SideBarPanel() {
 		super();
 		
 		setBackground(new Color(173, 233, 255));
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+		
+		Border customBorder = BorderFactory.createMatteBorder(0, 1, 1, 1, Color.BLACK);
+		
+		homeLabel = new JLabel("Home");
+		homeButton = new JButton();
+		homeButton.add(homeLabel);
+		setLabelDimension(homeButton);
         noleggioLabel = new JLabel("Noleggio");
         noleggioButton = new JButton();
         noleggioButton.add(noleggioLabel);
-        Border customBorder = BorderFactory.createMatteBorder(0, 1, 1, 1, Color.BLACK);
+        setLabelDimension(noleggioButton);
+        
+        
         setBorder(customBorder);
+        add(homeButton);
         add(noleggioButton);
         
 	}
@@ -43,5 +58,26 @@ public class SideBarPanel extends JPanel{
 	public void setNoleggioButton(JButton noleggioButton) {
 		this.noleggioButton = noleggioButton;
 	}
+
+	public JLabel getHomeLabel() {
+		return homeLabel;
+	}
+
+	public void setHomeLabel(JLabel homeLabel) {
+		this.homeLabel = homeLabel;
+	}
+
+	public JButton getHomeButton() {
+		return homeButton;
+	}
+
+	public void setHomeButton(JButton homeButton) {
+		this.homeButton = homeButton;
+	}
+	
+    private void setLabelDimension(JButton button) {
+    	button.setMaximumSize(new Dimension(120, 30));
+        button.setMinimumSize(new Dimension(120, 30));
+    }
 	
 }
