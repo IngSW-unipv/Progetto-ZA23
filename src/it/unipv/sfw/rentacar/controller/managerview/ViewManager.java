947c450af0aa461c8034406d23a3ffb191c8e070
Pitorac Roberto <razvanroberto.pitorac01@universitadipavia.it>
2024-09-10 22:44:20 +0200

    Aggiunta Metodi

--- src/it/unipv/sfw/rentacar/controller/managerview/ViewManager.java
+++ src/it/unipv/sfw/rentacar/controller/managerview/ViewManager.java
@@ -7,15 +7,19 @@ import javax.swing.UnsupportedLookAndFeelException;
 import it.unipv.sfw.rentacar.controller.accesso.RegistrationController;
 import it.unipv.sfw.rentacar.controller.accesso.LoginController;
 import it.unipv.sfw.rentacar.controller.catalogoauto.CatalogoAutoController;
-import it.unipv.sfw.rentacar.controller.clienti.AreaPersonaleController;
-import it.unipv.sfw.rentacar.controller.clienti.CambioPasswordController;
-import it.unipv.sfw.rentacar.controller.clienti.RinnovoPatenteController;
 import it.unipv.sfw.rentacar.controller.homepage.HomepageController;
+import it.unipv.sfw.rentacar.controller.utenti.amministratori.AggiungiAutoController;
+import it.unipv.sfw.rentacar.controller.utenti.amministratori.RimuoviAutoController;
+import it.unipv.sfw.rentacar.controller.utenti.clienti.AreaPersonaleController;
+import it.unipv.sfw.rentacar.controller.utenti.clienti.CambioPasswordController;
+import it.unipv.sfw.rentacar.controller.utenti.clienti.RinnovoPatenteController;
 import it.unipv.sfw.rentacar.model.agenzia.AgenziaNoleggioAuto;
 import it.unipv.sfw.rentacar.view.accesso.LoginUI;
 import it.unipv.sfw.rentacar.view.accesso.RegistrationUI;
 import it.unipv.sfw.rentacar.view.catalogoauto.CatalogoAutoUI;
 import it.unipv.sfw.rentacar.view.homepage.HomepageUI;
+import it.unipv.sfw.rentacar.view.utenti.amministratori.AggiungiAutoUI;
+import it.unipv.sfw.rentacar.view.utenti.amministratori.RimuoviAutoUI;
 import it.unipv.sfw.rentacar.view.utenti.clienti.AreaPersonaleUI;
 import it.unipv.sfw.rentacar.view.utenti.clienti.CambioPasswordUI;
 import it.unipv.sfw.rentacar.view.utenti.clienti.RinnovoPatenteUI;
@@ -52,5 +56,13 @@ public class ViewManager {
 	
 	public void passaAlRinnovoPatenteUI(AgenziaNoleggioAuto agenzia) throws IOException {
 		new RinnovoPatenteController(agenzia, new RinnovoPatenteUI());
+	}
+	
+	public void passaAdAggiungiAutoUI(AgenziaNoleggioAuto agenzia) throws IOException {
+		new AggiungiAutoController(agenzia, new AggiungiAutoUI());
+	}
+	
+	public void passaARimuoviAutoUI(AgenziaNoleggioAuto agenzia) throws IOException {
+		new RimuoviAutoController(agenzia, new RimuoviAutoUI());
 	}
 }
